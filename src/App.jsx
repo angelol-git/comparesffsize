3dc261d237ce096449e58aee397737be402028a8
angelol-git <ang.ladores.dev@gmail.com>
2025-03-31 17:40:11 -0400

    fetch data from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Canvas } from "@react-three/fiber";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import AddForm from "./components/sidebar/AddForm";
 import "./App.css";
 import "./reset.css";
 
+const queryClient = new QueryClient();
 function App() {
-  const [currentSideBarTab, setCurrentSideBarTab] = useState(0);
+  const [showAddForm, setShowAddForm] = useState(false);
 
   return (
-    <div className="container">
-      <h1>Compare Box Size</h1>
-      <main className="main-container">
-        <section id="canvas-wrapper" className="main-column canvas-wrapper">
-          {/* <Canvas>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <h1>Compare Box Size</h1>
+        <main className="main-container">
+          <section id="canvas-wrapper" className="main-column canvas-wrapper">
+            {/* <Canvas>
             <mesh>
               <boxGeometry args={[4, 2, 2]} />
               <meshStandardMaterial />
@@ -20,25 +23,26 @@ function App() {
           </Canvas>
           <ambientLight intensity={0.1} />
           <directionalLight position={[0, 0, 5]} color="red" /> */}
-        </section>
-        <section id="sidebar-wrapper" className="main-column sidebar-wrapper">
-          <div className="sidebar-container">
-            <header className="sidebar-header">
-              <h2>Items</h2>
-              <button
-                // onClick={() => {
-                //   setCurrentSideBarTab(1);
-                // }}
-                className="sidebar-nav-list-item-button"
-              >
-                Add Item +
-              </button>
-            </header>
-            <div>{currentSideBarTab === 0 && <AddForm />}</div>
-          </div>
-        </section>
-      </main>
-    </div>
+          </section>
+          <section id="sidebar-wrapper" className="main-column sidebar-wrapper">
+            <div className="sidebar-container">
+              <header className="sidebar-header">
+                <h2>Items</h2>
+                <button
+                  onClick={() => {
+                    setShowAddForm(true);
+                  }}
+                  className="sidebar-nav-list-item-button"
+                >
+                  Add Item +
+                </button>
+              </header>
+              <div>{showAddForm === true && <AddForm />}</div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/sidebar/AddForm.jsx
+++ src/components/sidebar/AddForm.jsx
@@ -1,11 +1,71 @@
+import { useState } from "react";
 import "./AddForm.css";
+import { useQuery } from "@tanstack/react-query";
 
-function handleSubmit(event) {
-  event.preventDefault();
+async function fetchCases() {
+  console.log("fetching case data");
+  const response = await fetch("/cases.json");
+  if (!response.ok) throw new Error("Network response was not ok");
+  const data = await response.json();
+  return data;
 }
 
 function AddForm() {
-  return <form id="add-form" onSubmit={handleSubmit}></form>;
+  const [category, setCategory] = useState("case");
+  const categories = ["case", "custom", "other"];
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["cases"],
+    queryFn: fetchCases,
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  return (
+    <form id="add-form" onSubmit={handleSubmit} className="add-form">
+      <div className="add-form-category-row">
+        <h3>Category:</h3>
+        <div>
+          {categories.map((categoryItem) => (
+            <label
+              key={categoryItem}
+              htmlFor={categoryItem}
+              className={`${category === categoryItem && "selected"}`}
+            >
+              <input
+                type="radio"
+                id={categoryItem}
+                name="category"
+                value={categoryItem}
+                checked={category === categoryItem}
+                onChange={(event) => {
+                  setCategory(event.target.value);
+                }}
+                className={`add-form-category-radio`}
+              />
+              {categoryItem.charAt(0).toUpperCase() + categoryItem.slice(1)}
+            </label>
+          ))}
+        </div>
+      </div>
+      <div>
+        <label htmlFor="name-select">Name: </label>
+        {isLoading && "Loading..."}
+        {error && "Error fetching cases"}
+        {data && (
+          <select name="cases" id="name-select">
+            {Object.entries(data).map(([brand, cases]) => {
+              return cases.map((caseItem, index) => (
+                <option key={`${brand}-${index}`} value={caseItem.name}>
+                  {`${brand} - ${caseItem.name}`}
+                </option>
+              ));
+            })}
+          </select>
+        )}
+      </div>
+    </form>
+  );
 }
 
 export default AddForm;
