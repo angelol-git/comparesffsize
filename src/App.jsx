b9eae667a14092aa786da426e36544e441eea034
angelol-git <ang.ladores.dev@gmail.com>
2025-04-08 18:59:29 -0400

    display selected objects in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { OrbitControls } from "@react-three/drei";
 import AddItemForm from "./components/sidebar/AddItemForm";
 import SelectedItems from "./components/sidebar/SelectedItems";
 import "./App.css";
@@ -17,13 +18,33 @@ function App() {
         <h1 className="main-header">Compare Box Size</h1>
         <div className="main-container">
           <section id="canvas-wrapper" className="canvas-wrapper">
-            <Canvas>
-              <mesh>
-                <boxGeometry args={[4, 2, 2]} />
-                <meshStandardMaterial />
+            <Canvas camera={{ position: [10, 10, 10], fov: 75 }}>
+              {selectedItems.length > 0 &&
+                selectedItems.map((item) => {
+                  return (
+                    <mesh>
+                      <boxGeometry
+                        args={[
+                          item.item.measurements.width / 30,
+                          item.item.measurements.height / 30,
+                          item.item.measurements.length / 30,
+                        ]}
+                      />
+                      <meshStandardMaterial color="red" />
+                    </mesh>
+                  );
+                })}
+              {/* <mesh>
+                <boxGeometry args={[4, 4, 4]} />
+                <meshStandardMaterial color="red" />
               </mesh>
-              <ambientLight intensity={0.1} />
-              <directionalLight position={[0, 0, 5]} color="red" />
+              <mesh position={[5, 0, 0]}>
+                <boxGeometry args={[4, 4, 4]} />
+                <meshStandardMaterial color="red" />
+              </mesh> */}
+              <directionalLight position={[4, 4, 1]} />
+              <ambientLight intensity={0.3} />
+              <OrbitControls />
             </Canvas>
           </section>
           <section id="sidebar-wrapper" className="sidebar-wrapper">
