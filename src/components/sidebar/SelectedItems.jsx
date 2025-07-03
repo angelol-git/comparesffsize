4100fa59ff343ed78f5a16b76419c450a4beecaf
angelol-git <ang.ladores.dev@gmail.com>
2025-07-03 19:35:18 -0400

    move animation code to own hook

--- src/components/sidebar/SelectedItems.jsx
+++ src/components/sidebar/SelectedItems.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import ItemForm from "./ItemForm/ItemForm";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import SelectedItemsOptions from "./SelectedItemOptions";
+import { useHoldDownAnimation } from "../../hooks/useHoldDownAnimation";
 function SelectedItems({
   item,
   setShowItemForm,
@@ -17,44 +18,7 @@ function SelectedItems({
   const [showSettingsModal, setShowSettingsModal] = useState(false);
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: item.id });
-
-  const dragHandleRef = useRef(null);
-  const holdStart = useRef(0);
-  const animationFrameId = useRef(null);
-  const [boxShadow, setBoxShadow] = useState("none");
-
-  function updateShadow() {
-    const duration = Math.min(Date.now() - holdStart.current, 300);
-    const spread = Math.min(duration / 60, 100);
-    setBoxShadow(`0 0 ${spread}px ${spread / 5}px rgba(0, 0, 0, 0.3)`);
-    animationFrameId.current = requestAnimationFrame(updateShadow);
-  }
-
-  function handlePointerDown() {
-    holdStart.current = Date.now();
-    animationFrameId.current = requestAnimationFrame(updateShadow);
-  }
-
-  function stopHolding() {
-    cancelAnimationFrame(animationFrameId.current);
-    setBoxShadow("none");
-  }
-
-  useEffect(() => {
-    const dragHandle = dragHandleRef.current;
-    if (!dragHandle) return;
-
-    dragHandle.addEventListener("pointerdown", handlePointerDown);
-    dragHandle.addEventListener("pointerup", stopHolding);
-    dragHandle.addEventListener("pointerleave", stopHolding);
-    dragHandle.addEventListener("pointercancel", stopHolding);
-    return () => {
-      dragHandle.removeEventListener("pointerdown", handlePointerDown);
-      dragHandle.removeEventListener("pointerup", stopHolding);
-      dragHandle.removeEventListener("pointerleave", stopHolding);
-      dragHandle.removeEventListener("pointercancel", stopHolding);
-    };
-  }, [dragHandleRef.current]);
+  const { boxShadow, dragHandleRef } = useHoldDownAnimation();
 
   function assignColor() {
     return item.hide ? "#4B4B4B" : item.color;
@@ -75,7 +39,6 @@ function SelectedItems({
     />
   ) : (
     <li
-      // style={style}
       style={{
         ...style,
         boxShadow: boxShadow,
